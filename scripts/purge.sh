164ee7c651cefe90ca358d571795f1a6adea42b2
aditya-arcot <arcot.aditya@utexas.edu>
2025-07-01 14:53:51 -0500

    init

---
+++ scripts/purge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -r ../node_modules
+rm -r ../shared/node_modules
+rm -r ../server/node_modules
+
+rm ../package-lock.json
+rm ../shared/package-lock.json
+rm ../server/package-lock.json
+
+rm -r ../shared/dist
+rm -r ../server/dist
